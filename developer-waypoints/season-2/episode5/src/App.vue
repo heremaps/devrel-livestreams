d9958186128d0086a5b2478d60d298eaf7158a93
Nic Raboy <nraboy@gmail.com>
2019-08-07 12:34:29 -0700

    add S02E05 code for Vue on Developer Waypoints

---
+++ developer-waypoints/season-2/episode5/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <HereMap
+            ref="map"
+            apiKey="HERE_API_KEY">
+        </HereMap>
+    </div>
+</template>
+
+<script>
+import HereMap from './components/HereMap.vue'
+
+export default {
+    name: 'app',
+    components: {
+        HereMap
+    },
+    mounted() {
+        let map = this.$refs.map;
+        map.dropMarker({ lat: 37.11, lng: -121.11 });
+        map.drawCircle({ lat: 37.11, lng: -121.11 }, 10000);
+        map.drawLine({ lat: 37.11, lng: -121.11 }, { lat: 37, lng: -121 });
+    }
+}
+</script>
+
+<style>
+    body {
+        margin: 0;
+    }
+</style>
